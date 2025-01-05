ebc34054655dc54b1a1b58567b739094503d73ea
Lucke Luz <luckeluz@hotmail.com>
2025-01-05 07:14:08 -0300

    feat: s2c51

--- code-snapshots/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,11 +1,15 @@
 import { Component, Input } from '@angular/core';
 
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [
+    TaskComponent,
+    NewTaskComponent
+  ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -37,11 +41,21 @@ export class TasksComponent {
     },
   ];
 
+  isAddingTask = false;
+
   get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userId);
   }
 
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask(cancel: boolean) {
+    this.isAddingTask = !cancel;
   }
 }
